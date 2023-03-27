c2c7a44fb96fc99fc7eba48cf622d5504d372d30
Aaron Demski <ajdemski@frontier.com>
2023-03-27 03:28:51 -0700

    final project with added css, javascript, and an updated readme

---
+++ README/README.md
@@ -0,0 +1,37 @@
+# _Language Suggester_
+
+_The Language Suggester is a project created by Aaron Demski. The repository includes index.html, css/styles.css, and the README folder. This website provides information all about me!_
+
+# _Technologies Used_
+
+_The project uses the following technologies:_
+
+# _HTML_
+# _CSS_
+# _Visual Studio Code_
+# _GitHub_
+
+## _Installation_
+
+_To use the Language Suggester project, follow these steps:_
+
+_1. Clone the repository to your local machine using Visual Studio Code._
+_2. Type git clone https://github.com/ajdemski/All-About-Me.git_
+_3.Navigate to the top level of the directory._
+_4.Open index.html in the cloned directory._
+
+## _Website_
+
+_You can view the website on Aaron Demski's GitHub page at the following link:_
+
+* _https://github.com/ajdemski/All-About-Me_
+
+## _Known Issues_
+
+_The Language Suggester project currently has no known bugs or issues._
+
+## _License_
+
+_The Language Suggester project is licensed under the MIT license. For more information, see the LICENSE file._
+
+(c) 2023 Aaron Demski. All rights reserved.
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,57 @@
-myForm.addEventListener('submit', onSubmit);
+//Business Logic
 
-function onSubmit(event) {
-    event.preventDefault();
+const languages = {
+ruby: "Ruby is a dynamic, open-source programming language that is easy to read and write.",
+javascript: "JavaScript is a high-level, interpreted programming language that is used to create interactive web pages and applications.",
+python: "Python is an interpreted, high-level, general-purpose programming language that is widely used in scientific computing, data analysis, and artificial intelligence.",
+};
 
-    if(answer1 === answer12 || answer12 === answer123 || answer123 === answer1)
-    msg.class.List.add('C#')
-    msg.innerHTML = 'C#';
-
-    setTimeout(() =>msg.remove(), 3000);
-    window.onload
+function generateOutput(experience, purpose, languages) {
+let output = "";
+output += `Based on your experience level of {experience} and your interest in {purpose}, we suggest you try using `;
+if (languages.length === 1) {
+output += languages[0];
+} else if (languages.length === 2) {
+output += `{languages[0]} and {languages[1]}`;
+} else if (languages.length === 3) {
+output += `{languages[0]}, {languages[1]}, and {languages[2]}`;
 }
+output += ". ";
+for (let i = 0; i < languages.length; i++) {
+output += `{languages[i]}: {languages[languages[i]]} `;
+}
+return output;
+}
+
+//User Interface Logic
+
+const experienceInput = document.getElementById("experience");
+const purposeInputs = document.getElementsByName("purpose");
+const languageInputs = document.getElementsByName("language");
+const submitButton = document.querySelector("button[type=submit]");
+const outputElement = document.getElementById("output");
+
+document.addEventListener("DOMContentLoaded", function() {
+const submitBtn = document.querySelector("button[type='submit']");
+const output = document.querySelector("#output");
+
+submitBtn.addEventListener("click", function() {
+const experience = document.querySelector("#experience").value;
+const purpose = document.querySelector("input[name='purpose']:checked").value;
+const languages = document.querySelectorAll("input[name='language']:checked");
+const languageValues = Array.from(languages).map(function(language) {
+return language.value;
+});
+
+let suggestedLanguages = [];
+if (experience === "none" && purpose === "web-development") {
+suggestedLanguages.push("JavaScript");
+} else if (purpose === "data-analysis") {
+suggestedLanguages.push("Python");
+} else {
+suggestedLanguages.push("Ruby", "Python");
+}
+
+output.textContent = `Based on your experience level, purpose, and preferred languages, we suggest the following language(s): {suggestedLanguages.join(", ")}.`;
+});
+});
